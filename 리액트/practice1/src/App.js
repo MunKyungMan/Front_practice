5f36f4cbfb23e7d45e44e36d5e9993678cde72a7
mungyeongman <oyesman1@naver.com>
2024-08-02 15:13:22 +0900

    Changes to be committed: 	modified:   "\353\246\254\354\225\241\355\212\270/practice1/src/App.css" 	modified:   "\353\246\254\354\225\241\355\212\270/practice1/src/App.js"

--- 리액트/practice1/src/App.js
+++ 리액트/practice1/src/App.js
@@ -1,18 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
 
   let post = '강남 우동 맛집';
+  let [글제목, b] = useState(['남자 코트 추천', '강남 우동맛집', '파이썬 독학']);
+  let [logo, setLogo] = useState('ReactBlog');
+
+  // let num = [1,2];
+  // let [a, c] = [1 , 2];
+  // let a = num[0];
+  // let c = num[1];
+
+
 
   return (
     <div className="App">
       <div className="black-nav">
-        <h4 id ={post} style={ {color : 'red', fontSize : '16px'}}>블로그</h4>
+        <h4>{logo}</h4>
       </div>
-      <h4>{ post }</h4>
+      <div className="list">
+        <h4>{ 글제목[0] }</h4>
+        <p>8월 2일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{ 글제목[1] }</h4>
+        <p>8월 2일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{ 글제목[2] }</h4>
+        <p>8월 2일 발행</p>
+      </div>
     </div>
   );
+
+  // return (
+  //   <div className="App">
+  //     <div className="black-nav">
+  //       <h4 id ={post} style={ {color : 'red', fontSize : '16px'}}>블로그</h4>
+  //     </div>
+  //     <h4>{ post }</h4>
+  //   </div>
+  // );
 }
 
 export default App;
