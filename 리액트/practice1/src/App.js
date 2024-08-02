6decaf6a23e347140ed4a711aca7678626b1ff97
mungyeongman <oyesman1@naver.com>
2024-08-02 16:17:04 +0900

    Changes to be committed: 	modified:   "\353\246\254\354\225\241\355\212\270/practice1/src/App.js"

--- 리액트/practice1/src/App.js
+++ 리액트/practice1/src/App.js
@@ -1,3 +1,5 @@
+// esLint-disable
+
 import logo from './logo.svg';
 import './App.css';
 import {useState} from 'react';
@@ -5,8 +7,15 @@ import {useState} from 'react';
 function App() {
 
   let post = '강남 우동 맛집';
-  let [글제목, b] = useState(['남자 코트 추천', '강남 우동맛집', '파이썬 독학']);
+  let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동맛집', '파이썬 독학']);
   let [logo, setLogo] = useState('ReactBlog');
+
+  let [좋아요, 좋아요변경] = useState(0);
+  
+
+  // function 함수(){
+  //   console.log(1);
+  // }
 
   // let num = [1,2];
   // let [a, c] = [1 , 2];
@@ -20,16 +29,21 @@ function App() {
       <div className="black-nav">
         <h4>{logo}</h4>
       </div>
+
+      <button onClick={()=>{
+        글제목변경( ['여자 코트 추천', '강남 우동맛집', '파이썬 독학'] )
+        }}>글수정</button>
+
       <div className="list">
-        <h4>{ 글제목[0] }</h4>
+        <h4>{ 글제목[0] } <span onClick={ ()=> {좋아요변경(좋아요+1)} }>👍</span> {좋아요} </h4>
         <p>8월 2일 발행</p>
       </div>
       <div className="list">
-        <h4>{ 글제목[1] }</h4>
+        <h4>{ 글제목[1] } <span onClick={ ()=> {좋아요변경(좋아요+1)} }>👍</span> {좋아요} </h4>
         <p>8월 2일 발행</p>
       </div>
       <div className="list">
-        <h4>{ 글제목[2] }</h4>
+        <h4>{ 글제목[2] } <span onClick={ ()=> {좋아요변경(좋아요+1)} }>👍</span> {좋아요} </h4>
         <p>8월 2일 발행</p>
       </div>
     </div>
