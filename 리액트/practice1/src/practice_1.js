a3cec492b3f2bb2a8ebd3e531e1e5aadc0d250ae
mungyeongman <oyesman1@naver.com>
2024-08-03 20:49:18 +0900

    Changes to be committed: 	new file:   "\353\246\254\354\225\241\355\212\270/practice1/src/practice_1.js"

---
+++ 리액트/practice1/src/practice_1.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function App() {
+    return (
+        <main>
+            <div>0</div>
+            <button>증가</button>
+        </main>
+    );
+}
+
+export default App;
