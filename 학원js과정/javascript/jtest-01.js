ec42bec0a147ebba0f0e2dad9d8779d2cf3590e3
mungyeongman <oyesman1@naver.com>
2024-08-05 22:50:25 +0900

    Changes to be committed: 	modified:   "\355\225\231\354\233\220js\352\263\274\354\240\225/javascript/jtest-01.js"

--- 학원js과정/javascript/jtest-01.js
+++ 학원js과정/javascript/jtest-01.js
@@ -1,21 +1,26 @@
 // 1. 클릭하면 1회 색상 변하기
-var head = document.querySelector("#heading");
-head.onclick = function(){
-    head.style.color = "red";
-}
+// var head = document.querySelector("#heading");
+// head.onclick = function(){
+//     head.style.color = "red";
+// }
 
-var hr_line = document.querySelector("#line");
-hr_line.onclick = function(){
-    hr_line.style.border = "20px solid blue";
-}
+// var hr_line = document.querySelector("#line");
+// hr_line.onclick = function(){
+//     hr_line.style.border = "20px solid blue";
+// }
 
 // 2. 반복적인 클릭을 통해서 모양을 바꿔보는 코드
 var head = document.querySelector("#heading");
-head.onclick = function(){
-    head.style.color = "red";
+var headOriginalColor = getComputedStyle(head).color; // 현재 스타일을 가져옴
+var headRed = false;
+
+head.onclick = function() {
+    if (headRed) {
+        head.style.color = headOriginalColor;
+        headRed = false;
+    } else {
+        head.style.color = "red";
+        headRed = true;
+    }
 }
 
-var hr_line = document.querySelector("#line");
-hr_line.onclick = function(){
-    hr_line.style.border = "20px solid blue";
-}
