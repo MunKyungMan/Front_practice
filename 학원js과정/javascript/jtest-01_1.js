af5691b7d502e18337bbb76283ccb3c0ae76ea79
mungyeongman <oyesman1@naver.com>
2024-08-06 23:03:21 +0900

    Changes to be committed: 	modified:   "\355\225\231\354\233\220js\352\263\274\354\240\225/CSS/jtest-02.css" 	modified:   "\355\225\231\354\233\220js\352\263\274\354\240\225/javascript/jtest-01_1.js" 	modified:   "\355\225\231\354\233\220js\352\263\274\354\240\225/jtest-01.html" 	modified:   "\355\225\231\354\233\220js\352\263\274\354\240\225/jtest-02.html"

--- 학원js과정/javascript/jtest-01_1.js
+++ 학원js과정/javascript/jtest-01_1.js
@@ -1,9 +1,10 @@
 document.write(
     `<body>
     <br>
-    <hr>
+    <hr style="border: 2px solid red; width: 500px; margin: 0px auto;">
     <h1 style='color:red; 
     border:3px solid black; 
+    border-radius: 20px;
     width: 400px; 
     height: auto; 
     margin: 0px auto; 
