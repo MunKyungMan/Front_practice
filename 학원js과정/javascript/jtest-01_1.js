e1f6c9c29cd4f759c097baa93ad611a2608b01ae
mungyeongman <oyesman1@naver.com>
2024-08-06 22:21:38 +0900

    Changes to be committed: 	modified:   "\355\225\231\354\233\220js\352\263\274\354\240\225/CSS/jtest-02.css" 	new file:   "\355\225\231\354\233\220js\352\263\274\354\240\225/javascript/jtest-01_1.js" 	modified:   "\355\225\231\354\233\220js\352\263\274\354\240\225/javascript/jtest-02.js" 	modified:   "\355\225\231\354\233\220js\352\263\274\354\240\225/jtest-01.html" 	modified:   "\355\225\231\354\233\220js\352\263\274\354\240\225/jtest-02.html"

---
+++ 학원js과정/javascript/jtest-01_1.js
@@ -0,0 +1,14 @@
+document.write(
+    `<body>
+    <br>
+    <hr>
+    <h1 style='color:red; 
+    border:3px solid black; 
+    width: 400px; 
+    height: auto; 
+    margin: 0px auto; 
+    margin-top: 50px;
+    background-color: yellowgreen;
+    text-align: center;'>웹문서꾸미기</h1>
+    </body>`
+);
