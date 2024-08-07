a05f25c4946845aab446705f3c0afefbfaf78677
mungyeongman <oyesman1@naver.com>
2024-08-07 22:20:38 +0900

    Changes to be committed: 	new file:   "\355\225\231\354\233\220js\352\263\274\354\240\225/javascript/jtest-02-1.js" 	modified:   "\355\225\231\354\233\220js\352\263\274\354\240\225/jtest-02.html"

---
+++ 학원js과정/javascript/jtest-02-1.js
@@ -0,0 +1,8 @@
+var user_name = prompt("이름 입력: ");
+alert(`${user_name}님 환영합니다.`);
+document.write(`<br><hr><span style = "color:blue;">${user_name}</span>님 환영합니다.`);
+console.log(`${user_name}님 환영합니다.`);
+
+var name_1 = prompt("이름:");
+var age = prompt("나이:");
+document.write(`<br><br>${name_1}님 나이는 ${age}세 입니다.`);
