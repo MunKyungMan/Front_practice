afc90de39301fe28348bf475ade847a2adc75560
mungyeongman <oyesman1@naver.com>
2024-08-07 22:45:30 +0900

    Changes to be committed: 	modified:   "\355\225\231\354\233\220js\352\263\274\354\240\225/javascript/jtest-02-1.js" 	new file:   "\355\225\231\354\233\220js\352\263\274\354\240\225/jtest-02-1.html"

--- 학원js과정/javascript/jtest-02-1.js
+++ 학원js과정/javascript/jtest-02-1.js
@@ -1,8 +1,26 @@
-var user_name = prompt("이름 입력: ");
-alert(`${user_name}님 환영합니다.`);
-document.write(`<br><hr><span style = "color:blue;">${user_name}</span>님 환영합니다.`);
-console.log(`${user_name}님 환영합니다.`);
+// var user_name = prompt("이름 입력: ");
+// alert(`${user_name}님 환영합니다.`);
+// document.write(`<br><hr><span style = "color:blue;">${user_name}</span>님 환영합니다.`);
+// console.log(`${user_name}님 환영합니다.`);
 
 var name_1 = prompt("이름:");
 var age = prompt("나이:");
-document.write(`<br><br>${name_1}님 나이는 ${age}세 입니다.`);
+document.write(`<div style = 
+    "border: 5px solid black; 
+    text-align: center; 
+    border-radius: 10px; 
+    margin: 50px auto; 
+    width: 400px; 
+    height: auto; 
+    background-color: green; 
+    color: white; 
+    font-weight: 800; 
+    padding: 10px;"
+    >`
+);
+document.write(
+    `<br><br><p style = "
+    font-size: 3em; 
+    line-height: 1.5em;
+    ">${name_1}님 나이는 ${age}세 입니다.</p>`);
+document.write(`</div>`);
