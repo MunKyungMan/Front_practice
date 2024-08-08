68d73663e157505dace1a69176484081d7bea2a4
mungyeongman <oyesman1@naver.com>
2024-08-09 00:05:20 +0900

    Changes to be committed: 	modified:   "\355\225\231\354\233\220js\352\263\274\354\240\225/CSS/jtest-04.css" 	new file:   "\355\225\231\354\233\220js\352\263\274\354\240\225/javascript/jtest-04-1.js" 	modified:   "\355\225\231\354\233\220js\352\263\274\354\240\225/javascript/jtest-04.js" 	new file:   "\355\225\231\354\233\220js\352\263\274\354\240\225/jtest-04-1.html" 	modified:   "\355\225\231\354\233\220js\352\263\274\354\240\225/jtest-04.html"

---
+++ 학원js과정/javascript/jtest-04-1.js
@@ -0,0 +1,3 @@
+// 연습문제
+var text1 = prompt("인사말");
+document.write(`<b><p style = "font-size: 3em; width: 400px; margin: 50px auto;">${text1}</p></b>`);
--- 학원js과정/javascript/jtest-04.js
+++ 학원js과정/javascript/jtest-04.js
@@ -1,3 +1,4 @@
 // 1. 자바스크립트 오류 찾기
-var name_1 = prompt("이름 입력:");
-document.write(`${name_1}님 회원가입 축하합니다.`);
+// var name_1 = prompt("이름 입력:");
+// document.write(`${name_1}님 회원가입 축하합니다.`); 
+
