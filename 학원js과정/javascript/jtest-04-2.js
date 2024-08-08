03504cb07275a1918c8d98139403bf1c76db181e
mungyeongman <oyesman1@naver.com>
2024-08-09 01:08:39 +0900

    Changes to be committed: 	new file:   "\355\225\231\354\233\220js\352\263\274\354\240\225/javascript/jtest-04-2.js" 	new file:   "\355\225\231\354\233\220js\352\263\274\354\240\225/jtest-04-2.html"

---
+++ 학원js과정/javascript/jtest-04-2.js
@@ -0,0 +1,32 @@
+// 연습문제
+var apple = "yammy";
+var banana;
+banana = "long and yammy";
+banana + apple
+document.write(`1. 바나나는 ${banana} 사과도${apple}`);
+
+var apple = 2000;
+var banana = 1000;
+var total = apple + banana;
+document.write(`<br>2. 총 구매 가격은 ${total}원 입니다.`);
+
+var apple = "2000";
+var banana = 1000;
+var total = apple - banana;
+document.write(`<br>3. 총 구매 가격은 ${total}원 입니다.`);
+
+var apple = "2000";
+var banana = 1000;
+var total = apple * banana;
+document.write(`<br>4. 총 구매 가격은 ${total}원 입니다.`);
+
+var apple = "2000";
+var banana = 1000;
+var total = apple / banana;
+document.write(`<br>5. 총 구매 가격은 ${total}원 입니다.`);
+
+var apple = 2000;
+var banana = 1000;
+var total = parseInt(apple) + banana;
+document.write(`<br>2. 총 구매 가격은 ${total}원 입니다.`);
+
