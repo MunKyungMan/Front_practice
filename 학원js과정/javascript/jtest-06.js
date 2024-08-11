62334dcc5207fb19dbbd222dbb5cde0deb867c77
mungyeongman <oyesman1@naver.com>
2024-08-11 16:08:50 +0900

    Changes to be committed: 	new file:   "\355\225\231\354\233\220js\352\263\274\354\240\225/CSS/jtest-06.css" 	new file:   "\355\225\231\354\233\220js\352\263\274\354\240\225/CSS/jtest-07.css" 	new file:   "\355\225\231\354\233\220js\352\263\274\354\240\225/javascript/jtest-06.js" 	new file:   "\355\225\231\354\233\220js\352\263\274\354\240\225/javascript/jtest-07.js" 	new file:   "\355\225\231\354\233\220js\352\263\274\354\240\225/jtest-06.html" 	new file:   "\355\225\231\354\233\220js\352\263\274\354\240\225/jtest-07.html"

---
+++ 학원js과정/javascript/jtest-06.js
@@ -0,0 +1,17 @@
+// document.querySelector("#result")
+
+// 1. 값이 정해진 상태에서 onclick이라는 이벤트가 발생이 되면, 
+// calc2()라는 사용자 함수를 호출해서 결과값을 표시하는 코드
+function calc2() {
+    var CurrentYear = 2024;
+    var BirthYear = parseInt(prompt("생년월일 입력: ", "20xx 혹은 19xx"));
+    var age;
+    age = CurrentYear - BirthYear + 1;
+
+    // 웹문서형식(글자+이미지 등...)으로 값을 전달할때에는 .innerHTML 코드를 사용한다.
+    // document.querySelector("#result").innerHTML = `당신의 나이는 ${age}세 입니다.`;
+
+    // 만약 오로지 텍스트 형식으로 값만 전달하고 싶을 때는 .innerHTML대신에 .innerText값을 넣는 것이 더 좋을 수도 있다.
+    document.querySelector("#result").innerText = `당신의 나이는 ${age}세 입니다.`;
+}
+
