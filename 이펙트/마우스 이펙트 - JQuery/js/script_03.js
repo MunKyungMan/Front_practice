2242479b1587d8802e14a3122f00def18abbce12
mungyeongman <oyesman1@naver.com>
2024-08-20 16:27:20 +0900

    Changes to be committed: 	modified:   "\354\235\264\355\216\231\355\212\270/\353\247\210\354\232\260\354\212\244 \354\235\264\355\216\231\355\212\270 - JQuery/index_01.html" 	modified:   "\354\235\264\355\216\231\355\212\270/\353\247\210\354\232\260\354\212\244 \354\235\264\355\216\231\355\212\270 - JQuery/index_03.html" 	modified:   "\354\235\264\355\216\231\355\212\270/\353\247\210\354\232\260\354\212\244 \354\235\264\355\216\231\355\212\270 - JQuery/js/script_03.js" 	modified:   "\354\235\264\355\216\231\355\212\270/\354\212\244\355\201\254\353\246\275\355\212\270 \354\235\264\355\216\231\355\212\270 - JavaScript/css/style_03.css" 	modified:   "\354\235\264\355\216\231\355\212\270/\354\212\244\355\201\254\353\246\275\355\212\270 \354\235\264\355\216\231\355\212\270 - JavaScript/index_03.html"

--- 이펙트/마우스 이펙트 - JQuery/js/script_03.js
+++ 이펙트/마우스 이펙트 - JQuery/js/script_03.js
@@ -1,11 +1,26 @@
+const width = $(".cursor").width() / 2;
+const height = $(".cursor").height() / 2;
+
 // 움직임 효과
-$(window).mousemove(function(e){
-    gsap.to(".cursor", {duration: 0.4, left: e.pageX, top: e.pageY});
+$(window).mousemove(function (e) {
+    gsap.to(".cursor", { duration: 0.4, left: e.pageX, top: e.pageY });
 });
 
 // 오버효과
-$(".mouseCont em").hover(function(){
+$(".mouseCont em").hover(function () {
     $(".cursor").addClass("active");
-}, function(){
+}, function () {
     $(".cursor").removeClass("active");
+});
+
+$(".info").hover(function () {
+    $(".cursor").addClass("show");
+}, function () {
+    $(".cursor").removeClass("show");
+})
+
+// 출력용
+$(window).mousemove(function (e) {
+    $(".pageX").text(e.pageX + "px");
+    $(".pageY").text(e.pageY + "px");
 });
